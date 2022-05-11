a71957f6c1a2086b2ee796fc1abc3c20bf7bd39b
yeriming <100740625+yeriming@users.noreply.github.com>
2022-05-11 17:58:39 +0900

    로그인과 회원가입 구현

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -9,6 +9,23 @@ axios.get('http://localhost:1337/api/movies').then(response => {
   console.log(response);
 });
 
+// Request API.
+axios
+  .post('http://localhost:1337/api/auth/local', {
+    identifier: 'user@strapi.io',
+    password: 'strapiPassword',
+  })
+  .then((response) => {
+    // Handle success.
+    console.log('Well done!');
+    console.log('User profile', response.data.user);
+    console.log('User token', response.data.jwt);
+  })
+  .catch((error) => {
+    // Handle error.
+    console.log('An error occurred:', error.response);
+  });
+
 
 const API_KEY = "c275787762fb2904adb52c4ad6412662";
 
